f38c86647470db97877ed04662b47950a77282bc
Tushar Kumar <tushargola1234@gmail.com>
2024-03-03 15:50:53 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://linkedin.com/in/tushar-kumar-4138701a1" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="https://instagram.com/iamtusharprajapati" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/tushar8057" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
